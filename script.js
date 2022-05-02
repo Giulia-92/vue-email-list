c4f5dc9e62dca584fefa135c5e9891b241cd3b79
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-02 16:45:41 +0200

    vue-email-list

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const app = new Vue({
     el:'#app',
     data:{
-    mymail:'',
+    mymail: [],
     },
     created(){
-        for(let i = 0; i< 10; i++){ 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
-            this.mymail = res.data.response;
-            console.log(res);
+        for(let i = 0; i < 10; i++){ 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                const mail = res.data.response;
+                this.mymail.push(mail);
+                //this.mymail = res.data.response; // miamail@mail.com
+                console.log(res);
             }).catch((error)=>{
-            console.log(error);
+                console.log(error);
             })
         }
     }
